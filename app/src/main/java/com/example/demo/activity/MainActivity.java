902fad69221ee960727081a8c12a5d0fd5903cb5
lenguyentrinh <lethinguyentrinh.work@gmail.com>
2024-05-14 17:22:45 +0700

    set up and  connect database

---
+++ app/src/main/java/com/example/demo/activity/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.demo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+
+import android.widget.TextView;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+
+import com.bumptech.glide.Glide;
+import com.example.demo.R;
+import com.example.demo.adapter.LoaiSpAdapter;
+import com.example.demo.model.LoaiSp;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewManHinhChinh;
+    NavigationView navigationView;
+    ListView listViewManHinhChinh;
+    DrawerLayout drawerLayout;
+    LoaiSpAdapter loaiSpAdapter;
+    List<LoaiSp> mangloaisp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Anhxa();
+        ActionBar();
+        ActionViewFlipper();
+
+
+    }
+
+    private void ActionBar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        }));
+
+    }
+    private void ActionViewFlipper(){
+        List<String> mangQuangCao = new ArrayList<>();
+        mangQuangCao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        mangQuangCao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
+        mangQuangCao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
+        for(int i = 0; i < mangQuangCao.size(); i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(mangQuangCao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setOutAnimation(slide_out);
+
+
+
+
+    }
+
+    private void Anhxa(){
+        toolbar = findViewById(R.id.toobarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewFlipper);
+        recyclerViewManHinhChinh = findViewById(R.id.recycleview);
+        listViewManHinhChinh = findViewById(R.id.listviewmanhinhchinh);
+        navigationView = findViewById(R.id.navigationview);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        //khoi tao list
+        mangloaisp = new ArrayList<>();
+        // khoi tao adapter
+        loaiSpAdapter  = new LoaiSpAdapter(getApplicationContext(),mangloaisp);
+        listViewManHinhChinh.setAdapter(loaiSpAdapter);
+
+    }
+}
